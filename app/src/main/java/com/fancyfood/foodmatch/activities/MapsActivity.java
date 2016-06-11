7b5d62c6a075d9722e5f3c083d89e29f87725e38
Carolina <s0544541@htw-berlin.de>
2016-06-11 21:44:19 +0200

    current location as StartPosition for route know error: crashes when gps is off

--- app/src/main/java/com/fancyfood/foodmatch/activities/MapsActivity.java
+++ app/src/main/java/com/fancyfood/foodmatch/activities/MapsActivity.java
@@ -1,8 +1,15 @@
 package com.fancyfood.foodmatch.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.akexorcist.googledirection.DirectionCallback;
@@ -24,28 +31,96 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DirectionCallback   {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DirectionCallback {
     //server Key for Google Direction API
     String serverKey = "AIzaSyBd65lCjwNb-1W03t4XZTfqQu9yjUhyssY";
-	//position of start and end address
-    private LatLng origin = new LatLng(52.455521, 13.526905);
+    //position of start and end address
+    private LatLng origin;
     //private LatLng destination= new LatLng(52.523986, 13.402637);
-	//focus start position
-    private LatLng camera = new LatLng(52.455521, 13.526905);
+    //focus start position
+    private LatLng camera;// = new LatLng(52.455521, 13.526905);
+    private LocationManager manager;
+    private LocationListener listener;
+    private String provider;
     public static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+
+    public void setOrigin(LatLng origin) {
+        this.origin = origin;
+    }
+
+    public void setCamera(LatLng camera) {
+        this.camera = camera;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-		//trigger von onMapReady
+        //trigger von onMapReady
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
         Intent DataIntent = getIntent();
         //get Date from MainActivity
-        LatLng destination= new LatLng(DataIntent.getDoubleExtra("Lat", 0), DataIntent.getDoubleExtra("Lng", 0));
-		//call requestDirection for calculation and drawing of the route
+        LatLng destination = new LatLng(DataIntent.getDoubleExtra("Lat", 0), DataIntent.getDoubleExtra("Lng", 0));
+
+        //get StartLocation
+        getCurrentLocation();
+
+        //call requestDirection for calculation and drawing of the route
         requestDirection(destination);
+    }
+
+    public void getCurrentLocation() {
+
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        provider = manager.getBestProvider(criteria, true);
+
+        listener = new LocationListener() {
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng origin;
+                if (location != null) {
+                    origin = new LatLng(location.getLatitude(), location.getLongitude());
+                } else {
+                    origin = new LatLng(52.455521, 13.526905);
+                }
+                setOrigin(origin);
+                setCamera(origin);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (manager.isProviderEnabled(provider)) {
+            manager.requestLocationUpdates(provider, 0, 0, listener);
+        }
     }
 	
     @Override
@@ -57,6 +132,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void requestDirection(LatLng destination){
 		//use the Android-GoogleDirectionLibrary option see http://www.akexorcist.com/2015/12/google-direction-library-for-android-en.html
+        if (origin==null)
+        {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            if (manager.isProviderEnabled(provider)){
+            manager.requestSingleUpdate(provider,listener, null);
+            }
+
+        }
+        if (origin==null)
+        {
+
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0, listener);
+        }
+        if (origin==null)
+        {
+            Location Loc=manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            setOrigin(new LatLng(Loc.getLatitude(), Loc.getLongitude()));
+            setCamera(new LatLng(Loc.getLatitude(), Loc.getLongitude()));
+        }
         GoogleDirection.withServerKey(serverKey)
                 .from(origin)		
                 .to(destination)
