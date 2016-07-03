8b5c53506188d53a6b1a84300862a585de6cd904
felix133711 <felixklemm@gmx.de>
2016-07-03 15:36:16 +0200

    added download function please review save functionallity error

--- app/src/main/java/com/fancyfood/foodmatch/activities/JSONActivity.java
+++ app/src/main/java/com/fancyfood/foodmatch/activities/JSONActivity.java
@@ -1,11 +1,15 @@
 package com.fancyfood.foodmatch.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fancyfood.foodmatch.R;
@@ -15,6 +19,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,6 +32,11 @@ import java.net.URL;
 public class JSONActivity extends AppCompatActivity {
 
     public TextView textView;
+    public ImageView imageView;
+
+    public String token = "n5DUfSC72hPABeEhu89Ex63soJ2oJCQfTxlim8MC6oHVLlrutMa3xDjDursL";
+    public String url = "http://api.collective-art.de/";
+
     private static final String LOG_TAG = JSONActivity.class.getSimpleName();
 
     @Override
@@ -34,24 +46,33 @@ public class JSONActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView)findViewById(R.id.imageView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                new JSONTask().execute("http://api.collective-art.de/restaurants?token=n5DUfSC72hPABeEhu89Ex63soJ2oJCQfTxlim8MC6oHVLlrutMa3xDjDursL");
+                new JSONTask().execute(url + "restaurants?token=" + token);
             }
         });
     }
 
     public class JSONTask extends AsyncTask<String, String, String> {
 
+        public Bitmap bitmap = null;
+
         // IntentService
+        protected String getFullUrl(String uri) {
+
+            return url  + uri + "?token=" + token;
+        }
+
         @Override
         protected String doInBackground(String... params) {
 
             HttpURLConnection connection = null;
             BufferedReader reader = null;
+
 
             try {
                 URL url = new URL(params[0]);
@@ -61,46 +82,56 @@ public class JSONActivity extends AppCompatActivity {
 
                 InputStream stream = connection.getInputStream();
 
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
+                if(stream != null) {
 
-                while ((line = reader.readLine()) != null) {
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
+                    String line = "";
 
-                    buffer.append(line);
+                    while ((line = reader.readLine()) != null) {
+
+                        buffer.append(line);
+                    }
+
+                    // check response code for 200
+                    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                        JSONObject parentObject = null;
+                        JSONArray parentArray = null;
+                        JSONObject finalObject = null;
+                        JSONArray finalArray = null;
+
+                        Log.d(LOG_TAG, "DIR: " + Environment.getExternalStorageDirectory());
+                        String finalJson = buffer.toString();
+                        parentArray = new JSONArray(finalJson);
+
+                        for(int i = 0;i < parentArray.length(); i++) {
+
+                            finalObject = parentArray.getJSONObject(i).getJSONObject("media");
+                            String fileName = finalObject.getString("file");
+
+                            Log.d(LOG_TAG, fileName);
+                            Log.d(LOG_TAG, getFullUrl("restaurants/media/" + fileName));
+
+                            URL imageurl = new URL(getFullUrl("restaurants/media/" + fileName));
+                            bitmap = BitmapFactory.decodeStream(imageurl.openConnection().getInputStream());
+
+                            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+
+                            File file = new File(Environment.getExternalStorageDirectory() + File.separator + fileName);
+                            file.createNewFile();
+
+                            FileOutputStream fileOutput = new FileOutputStream(file);
+
+                            fileOutput.write(bytes.toByteArray());
+                            fileOutput.close();
+                        }
+                        return finalJson;
+                    }
+
                 }
 
-                // check response code
-                if (connection.getResponseCode() == 200) {
-                    String finalJson = buffer.toString();
-                    JSONArray parentArray = new JSONArray(finalJson);
-                    return finalJson;
-                }
-
-                JSONArray parentArray = null;
-                JSONObject parentObject = null;
-                String restaurantName;
-
-                /*
-                if (String.valueOf(finalJson.charAt(0)) == "[") {
-                    Log.d(LOG_TAG, "its an array");
-                    parentArray = new JSONArray(finalJson);
-                } else {
-                    parentObject = new JSONObject(finalJson);
-                }
-                */
-
-                /*
-                for (int i = 0; i < parentArray.length();i++) {
-
-                    Log.d(LOG_TAG, " "+i);
-                    JSONObject firstIndex = parentArray.getJSONObject(i);
-
-                    restaurantName = firstIndex.getString("name");
-
-                    Log.d(LOG_TAG, restaurantName);
-                }
-                */
 
                 //Log.(LOG_TAG,"test" + finalJson);
                 /*
@@ -119,7 +150,6 @@ public class JSONActivity extends AppCompatActivity {
                 */
 
             } catch (MalformedURLException e) {
-
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -145,6 +175,7 @@ public class JSONActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             textView.setText(result);
+            imageView.setImageBitmap(bitmap);
         }
     }
 }
